2d18ce9484386139fbd1ac1ebbd8547a495fd63b
Rachel Kennelly <rekennelly@gmail.com>
2020-12-14 14:10:50 -0500

    GUI designed & built

--- src/rsa/RSA.java
+++ src/rsa/RSA.java
@@ -1,21 +1,30 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package rsa;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 /**
  *
  * @author kennelra
  */
-public class RSA {
+public class RSA extends Application {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("RSAFXML.fxml"));
+        Scene scene = new Scene(root);
+        stage.setTitle("RSA Encryption Key Generator");
+        stage.setScene(scene);
+        stage.show();
     }
+    
+    // main ----------------------------------
+    public static void main(String[] args) {
+        launch(args);
+    } // end main
     
 }
